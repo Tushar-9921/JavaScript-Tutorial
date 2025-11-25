3a40fac2f9da721ccd3333e12b87cb1dadf6f601
Tushar Parmar <trparmar.3480@gmail.com>
2025-11-25 04:34:05 +0000

    Datatypes

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,35 @@
+"use strict"  // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    + 
+    3) // code readability should be high 
+
+
+let name = "Tushar"
+let age = 19
+let isLoggedIn = true
+let userState; // it is undefined meanse not assigned value
+let userCity = null; // this is a value for empty 
+
+// number => 2 to power 52
+// bigint => when store large number
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => not define value
+// symbol => unique 
+
+
+// Object
+
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+console.log(typeof name);
+console.log(typeof userState);
+console.log(typeof userCity);
+
+
+console.log(null)   // null is an object
+console.log(undefined)  // undefined is a type
