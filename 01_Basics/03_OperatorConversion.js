b6dc97630556b76a3ffdf6ce75d4830e50ca8735
Tushar Parmar <trparmar.3480@gmail.com>
2025-11-26 15:52:39 +0000

    Comparision Operator and Conversion

--- 01_Basics/03_OperatorConversion.js
+++ 01_Basics/03_OperatorConversion.js
@@ -6,11 +6,9 @@ let checkNumber = Number(score)
 // console.log(typeof score)
 // console.log(checkNumber)
 
-
 // "33" => 33
 // "33adf" => NaN
 // true => 1 , false => 0
-
 
 let isLoggedIn = 1
 let boolean = Boolean(isLoggedIn)
@@ -24,5 +22,50 @@ let boolean = Boolean(isLoggedIn)
 let number = 33
 
 let numberToString = String(number)
-console.log(number)
-console.log(typeof number)
+// console.log(number)
+// console.log(typeof number)
+
+// ***************************** Operations ******************************** //
+
+let value = 2
+let negvalue = -value
+
+// console.log(negvalue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/2)
+// console.log(2%2) (most usage of %)
+
+// console.log(2 + 2)
+// console.log("2" + 2)
+// console.log(1 + 2 + "2")
+// console.log(2 + 2 + "2" + 2 + 2)
+// console.log("2" + "2")
+
+// console.log((3 + 4) * 5 % 3) // must be prefer (this)
+
+// console.log(+true)
+// console.log("" +)
+
+let num1, num2, num3
+
+// num1 = num2 = num3 = 2 + 2  // Not prefer this type because code is not readable for all
+
+// let getCounter = 100
+// ++getCounter; 
+// console.log(getCounter);
+
+// prefix
+let x = 4
+const y = ++x
+console.log(x)
+console.log(y)
+
+// postfix
+let a = 4
+const b = x++
+console.log(x)
+console.log(y)
---
+++ 01_Basics/04_Comparision.js
@@ -0,0 +1,33 @@
+// console.log(2 > 1) // true
+// console.log(2 >= 1) // true
+// console.log(2 < 1) // false
+// console.log(2 <= 1) // false
+// console.log(2 == 1) // false
+// console.log(2 != 1) //true
+
+// ******* Avoid this type of conversation because it is not good it is very confusing
+
+
+// console.log("2" >= 2) // Must be prefer same datatype when we comparision 
+// console.log(2 < "2")
+
+// console.log(null > 0)
+// console.log(null == 0)
+// console.log(null < 0)
+// console.log(null <= 0) true
+// console.log(null >= 0) true
+
+
+// console.log(undefined == 0)
+// console.log(undefined >= 0)
+// console.log(undefined <= 0)
+// console.log(undefined != 0) // true
+// console.log(undefined < 0)
+// console.log(undefined > 0)
+
+// **************************************************************************************
+
+
+// === (check with datatye and strictly check)
+
+console.log("2" === 2)
