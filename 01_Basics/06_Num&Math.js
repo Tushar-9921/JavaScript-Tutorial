d13cacb87f0d7552e5868794f9ad42774e150fb1
Tushar Parmar <trparmar.3480@gmail.com>
2025-11-28 05:58:55 +0000

    Learn about Numbers

---
+++ 01_Basics/06_Num&Math.js
@@ -0,0 +1,18 @@
+const score = 1050
+// console.log(score)
+
+const bankBalance = new Number(400) // Number is an object 
+// console.log(bankBalance)
+// console.log(typeof bankBalance)
+// console.log(bankBalance.toFixed(2)) 
+
+const anotherNumber = new Number(54323.553587)
+// console.log(anotherNumber.toPrecision(7))
+
+const secondNumber = 780000
+// console.log(secondNumber.toLocaleString('en-IN'))
+
+// ++++++++++++++++++++++++++++++++++++++ Maths +++++++++++++++++++++++++++++++++++++++++++
+
+
+console.log(Math)
