d920e0362f6c6db9e9987fd3c3f963d12d4e189c
Tushar Parmar <trparmar.3480@gmail.com>
2025-11-28 04:07:23 +0000

    String basics

--- 01_Basics/05_String.js
+++ 01_Basics/05_String.js
@@ -1,17 +1,44 @@
 const name = "Tushar"
-const num = 14
+const myPass = "9921"
 
-// console.log("My name is: " + name + " Your Number is: " + num)
+// console.log("My name is : " + name + " Your Password is: " + myPass)
 
-// console.log(`My name is: ${name} My Number is: ${num}`)
+console.log(`My name is : ${name.toUpperCase()} , and password is ${myPass}`)
+// console.log(typeof myPass)
 
-const emailId = new String("tushar@gmail.com")
-console.log(emailId[0]) // access Key Value pair
-console.log(emailId.__proto__) // access Prototype
+const myGame = new String("Gta-5-v-inhance-version") // String is an Object
 
-console.log(typeof emailId)
+console.log(myGame)
+console.log(myGame[2]) // Key,Value pair access
+console.log(typeof myGame); // String as Object
+console.log(myGame.__proto__) // access Prototype
 
-console.log(name.toUpperCase()) // Not change Original value because it is Primitive Datatype
+console.log(myGame.toLowerCase()) // Access String Methods
+console.log(myGame.toUpperCase()) 
+console.log(myGame.at(4))
+console.log(myGame.charAt(2)) // Give number to show his Character
+console.log(myGame.indexOf('t')) // Give Character to show his length Number
+// console.log(myGame.padEnd())
+// console.log(myGame.lastIndexOf(5))
+console.log(myGame.bold())
+console.log(myGame.fontcolor("red"))
 
-console.log(name.charAt(4))
-console.log(name.indexOf("T"))
+// Slice String
+
+const sliceStr = myGame.substring(0,3)
+console.log(sliceStr)
+
+const anotherStr = myGame.slice(-5, 2)
+console.log(anotherStr)
+
+
+const myNewString = "     Bhavesh     "
+console.log(myNewString)
+console.log(myNewString.trim()) // Remove space from both side
+
+const url = "https://trparmar%209921/home/repo"
+console.log(url.replace('%20','-')) // Replace string
+console.log(url.includes("9921")) // include or not
+
+
+console.log(myGame.split('-')) // String convert into array
